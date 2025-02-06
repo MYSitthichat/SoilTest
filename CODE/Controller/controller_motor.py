1cb2734964a7ee95f99bef9fc84b7e6dd6eff5c4
MYSitthichat <bbgan.com@gmail.com>
2025-02-06 23:46:52 +0700

    backup file

--- CODE/Controller/controller_motor.py
+++ CODE/Controller/controller_motor.py
@@ -1,12 +1,13 @@
 from PySide6.QtCore import QThread, Signal,QObject
 import serial as ser
+import time
 
-class ControllerArduino(QThread,QObject):
+class ControllerMotor(QThread,QObject):
     displace_xy_data = Signal(list)
-    connect_serial_arduino_signal = Signal()
+    connect_serial_motor_signal = Signal()
 
     def __init__(self):    
-        super(ControllerArduino, self).__init__()
+        super(ControllerMotor, self).__init__()
         self.running = True
         self.ar_data = ""
         self.ar_x = ""
@@ -23,58 +24,75 @@ class ControllerArduino(QThread,QObject):
     def run(self):
         while self.running:
             try:
-                self.ar_data = (self.arduino_serial.readline().decode('utf-8')).strip()
-                if self.ar_data != "" :
-                    if len(self.ar_data.split(",")) == 2: 
-                        self.ar_data = self.ar_data.split(",")
-                        self.ar_x = self.ar_data[1]
-                        self.ar_y = self.ar_data[0]
-                        self.disxy_data = [self.ar_x,self.ar_y]
-                        self.displace_xy_data.emit(self.disxy_data)
-                    else:
-                        self.ar_data = ""
-                else:
-                    self.ar_data = ""
+                pass
             except Exception as e:
                 pass
-            self.msleep(100)  
+            self.msleep(1000)  
 
     def stop(self):
         self.running = False
-        self.arduino_serial.close()
+        self.motor_serial.close()
         self.wait()
         
     
-    def get_comport(self,Arduino_comport):
-        self.arduino_serial = ser.Serial(port=Arduino_comport, baudrate=9600, timeout=1)
-        if self.arduino_serial.is_open:
-            self.connect_serial_arduino_signal.emit()
+    def get_comport(self,motor_comport):
+        self.motor_serial = ser.Serial(port=motor_comport, baudrate=9600, timeout=1)
+        if self.motor_serial.is_open:
+            self.connect_serial_motor_signal.emit()
+            print(self.motor_serial.is_open)
         else:
             print("Serial Arduino not connected")
+            
+    # ====================== SET SPEED MOTOR ======================
+    
+    def get_setting_speed(self,speedx,speedy):
+        speedx = int(speedx)
+        speedy = int(speedy)
+        try:
+            if speedx > 100:
+                speedx = 100
+                speedx = str(speedx)
+            if speedy > 100:
+                speedy = 100
+                speedy = str(speedy)
+            speedx = str(speedx)
+            speedy = str(speedy)
+            self.set_speed_motor_x = (b'p1'+speedx.encode()+b'\n')
+            self.set_speed_motor_y = (b'p2'+speedy.encode()+b'\n')
+            # print(self.set_speed_motor_x)
+            # print(self.set_speed_motor_y)
+            self.motor_serial.write(self.set_speed_motor_x)
+            time.sleep(0.5)
+            self.motor_serial.write(self.set_speed_motor_y)
+            time.sleep(0.5)
+            self.stop_all_motors()
+        except Exception as e:
+            print(e)
+            
     # ====================== Y ======================
     def up_motors_y(self):
-        self.arduino_serial.write(self.m_up_my)
+        self.motor_serial.write(self.m_up_my)
         
     def down_motors_y(self):
-        self.arduino_serial.write(self.m_down_my)
+        self.motor_serial.write(self.m_down_my)
     
     def stop_motors_y(self):
-        self.arduino_serial.write(self.stop_my)
+        self.motor_serial.write(self.stop_my)
     # ====================== X ======================
     def in_motors_x(self):
-        self.arduino_serial.write(self.m_in_mx)
+        self.motor_serial.write(self.m_in_mx)
     
     def out_motors_x(self):
-        self.arduino_serial.write(self.m_out_mx)
+        self.motor_serial.write(self.m_out_mx)
     
     def stop_motors_x(self):
-        self.arduino_serial.write(self.stop_mx)
+        self.motor_serial.write(self.stop_mx)
     # ====================== SA ======================
     def stop_all_motors(self):
-        self.arduino_serial.write(self.stop_all_m)
+        self.motor_serial.write(self.stop_all_m)
     # ==============================================
-        
     
+
 
 # help          - Display this help message
 # m1            - Start Motor 1 clockwise
